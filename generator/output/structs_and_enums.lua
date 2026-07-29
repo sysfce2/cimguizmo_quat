1239151198b7a5c6fb22dd68bc49648fb7125c0c
sonoro1234 <sonoroster@gmail.com>
2026-07-29 10:51:10 +0200

    generate

--- generator/output/structs_and_enums.lua
+++ generator/output/structs_and_enums.lua
@@ -157,6 +157,7 @@ local t={
     ImGuiOldColumnData="inherited",
     ImGuiOldColumns="inherited",
     ImGuiOnceUponAFrame="inherited",
+    ImGuiPackedDate="inherited",
     ImGuiPayload="inherited",
     ImGuiPlatformIO="inherited",
     ImGuiPlatformImeData="inherited",
